6fe7b8cfa3a2e052568ee0e77f0e79f11a4cee46
SGerenaR <soni.gerena@gmail.com>
2016-03-26 16:19:36 +1000

    homepage goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def hello
-  	render text: "hola, Mundo!"
+  #def hello
+  #	render text: "hola, Mundo!"
+  #end
+
+  def goodbye
+render text: “goodbye, world!”
   end
 end
